794904d70121029ceb3222878d2a5286808013e1
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-12-03 21:46:11 +0100

    Day 3 part 1 update.

--- src/03/part1.nim
+++ src/03/part1.nim
@@ -1,46 +1,47 @@
 import nre
 import strutils
 import sequtils
-import tables
-import math
 
 type
-   Coordinate = tuple
-      x, y: int
-
    Claim = object
       id: int
-      top_left, bottom_right: Coordinate
+      left, right: int # X coordinates
+      top, bottom: int # Y coordinates
 
-   Fabric = CountTable[Coordinate]
+   Fabric = array[0..1000, array[0..1000, int]]
 
 
 proc parse_claim(claim: string): Claim =
    ## Parse a claim from a string.
-   let captures = nre.match(claim, re"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").get.captures
+   let captures = nre.match(claim,
+                            re"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").get.captures
    let num = map(captures.to_seq(), parse_int)
    result.id = num[0]
-   result.top_left = (num[1], num[2])
-   result.bottom_right = (num[1] + num[3] - 1, num[2] + num[4] - 1)
+   result.left = num[1]
+   result.right = num[1] + num[3] - 1
+   result.top = num[2]
+   result.bottom = num[2] + num[4] - 1
 
 
-proc mark_fabric(fabric: var Fabric, claim: Claim) =
-   for x in countup(claim.top_left.x, claim.bottom_right.x):
-      for y in countup(claim.top_left.y, claim.bottom_right.y):
-         inc(fabric, (x, y))
+proc mark_fabric(fabric: var Fabric, claim: Claim): int =
+   ## Mark the claim in the fabric. Returns the number of square inches that
+   ## uniquely overlaps with another claim.
+   for x in countup(claim.left, claim.right):
+      for y in countup(claim.top, claim.bottom):
+         if fabric[x][y] == 1:
+            inc(result)
+         inc(fabric[x][y])
 
 
 proc get_total_overlapping_area(filename: string): int =
+   ## Mark the fabric with the claims defined in the input file and return the
+   ## number of square inches marked by two or more claims.
    let lines = read_file(filename).strip().split_lines()
    let claims = map(lines, parse_claim)
-   var fabric = init_count_table[Coordinate](2 ^ 20)
+   var fabric: Fabric
 
    for c in claims:
-      mark_fabric(fabric, c)
-
-   for val in values(fabric):
-      if val > 1:
-         inc(result)
+      inc(result, mark_fabric(fabric, c))
 
 
 when is_main_module:
