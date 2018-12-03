b71512d65f2e188350f5b71d585415274f3be09a
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-12-03 21:59:55 +0100

    Day 3 part 2 update.

--- src/03/part2.nim
+++ src/03/part2.nim
@@ -5,44 +5,39 @@ import tables
 import math
 
 type
-   Coordinate = tuple
-      x, y: int
-
    Claim = object
       id: int
-      top_left, bottom_right: Coordinate
-
-   Fabric = CountTable[Coordinate]
+      left, right: int # X coordinates
+      top, bottom: int # Y coordinates
 
 
 proc parse_claim(claim: string): Claim =
    ## Parse a claim from a string.
-   let captures = nre.match(claim, re"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").get.captures
+   let captures = nre.match(claim,
+                            re"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").get.captures
    let num = map(captures.to_seq(), parse_int)
    result.id = num[0]
-   result.top_left = (num[1], num[2])
-   result.bottom_right = (num[1] + num[3] - 1, num[2] + num[4] - 1)
+   result.left = num[1]
+   result.right = num[1] + num[3] - 1
+   result.top = num[2]
+   result.bottom = num[2] + num[4] - 1
 
 
 proc is_overlapping(x, y: Claim): bool =
    ## Returns true if the claims ``x`` and ``y`` overlap.
-   if y.top_left.x > x.bottom_right.x:
-      return false
-   elif y.bottom_right.x < x.top_left.x:
-      return false
-   elif y.top_left.y > x.bottom_right.y:
-      return false
-   elif y.bottom_right.y < x.top_left.y:
-      return false
-   else:
-      return true
+   result = not (y.left > x.right or
+                 y.right < x.left or
+                 y.top > x.bottom or
+                 y.bottom < x.top)
 
 
 proc get_intact_claim_id(filename: string): int =
+   ## Return the ID of the claim that doesn't overlap with any other claim.
    let lines = read_file(filename).strip().split_lines()
    let claims = map(lines, parse_claim)
 
-   # Mark the fabric just as before.
+   # Check each claim against every other claim. This could obviously be
+   # improved.
    for i in 0..<len(claims):
       var overlap = false
       for j in 0..<len(claims):
@@ -56,4 +51,5 @@ proc get_intact_claim_id(filename: string): int =
 
 
 when is_main_module:
-   echo "The ID of the intact claim is ", get_intact_claim_id("input.txt"), ".\n"
+   echo "The ID of the intact claim is ",
+        get_intact_claim_id("input.txt"), ".\n"
